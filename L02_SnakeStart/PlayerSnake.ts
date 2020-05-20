6a79a289d9ca83a26c2b6aa90a252f6a0502f193
flori <florian-pfuetzenreuter@hotmail.de>
2020-05-20 18:16:31 +0200

    removed buttons for mobile test

--- L02_SnakeStart/PlayerSnake.ts
+++ L02_SnakeStart/PlayerSnake.ts
@@ -6,8 +6,6 @@ namespace L02_SnakeStart {
 		
 		private cameraMirror: FudgeCore.ComponentCamera;
 		private componentCamera: FudgeCore.ComponentCamera;
-		private buttonLeft : HTMLButtonElement;
-		private buttonRight : HTMLButtonElement;
 
 		constructor(
 			wallSegments : f.Node[],
@@ -18,22 +16,7 @@ namespace L02_SnakeStart {
 			super(wallSegments, obstacleSegments, collectibleElements, collectibles);
 			this.initCameraEgo();
 			this.initCameraMirror();
-			this.buttonLeft = <HTMLButtonElement>document.getElementById("left");
-			this.buttonRight = <HTMLButtonElement>document.getElementById("right");
-			this.initButtonHandler();
 			window.addEventListener("keydown", (event) => { this.keyDownHandler(event); });
-		}
-
-		private initButtonHandler() : void {
-			this.buttonRight.addEventListener("mousedown", (event)=>{
-				event.preventDefault();
-				this.moveHeadRight();
-			})
-
-			this.buttonLeft.addEventListener("mousedown", ()=>{
-				event.preventDefault();
-				this.moveHeadLeft();
-			})
 		}
 
 		private keyDownHandler(event : KeyboardEvent) : void {
